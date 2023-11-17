c564fe3b2767b67737af0dc81d6af395fe5f6b64
berrybab6 <bedatuassefa@gmail.com>
2023-11-17 15:47:29 +0300

    WIP bot-development

--- api/index.py
+++ api/index.py
@@ -1,31 +1,57 @@
-# from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-# import os
+import os
+from typing import Optional
+from fastapi import FastAPI,Request
+from pydantic import BaseModel
 
-# TOKEN = os.environ.get("TOKEN")
+# TELEGRAM
+from telegram import Update,Bot
+from telegram.ext import Dispatcher,MessageHandler ,Filters,CommandHandler
 
-# def start(update, context):
-#     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-# def register_handlers(dispatcher):
-#     start_handler = CommandHandler('start', start)
-#     dispatcher.add_handler(start_handler)
 
-# def main():
-#     updater = Updater(token=TOKEN, use_context=True)
-#     dispatcher = updater.dispatcher
+TOKEN = os.environ.get("TOKEN")
 
-#     register_handlers(dispatcher)
+TOKEN = "6121025171:AAEF2n8BfnmWsrp_vkajprGrffwBFYAtpFg"
+app =FastAPI()
 
-#     updater.start_polling()
-#     updater.idle()
+class TelegramWebHook(BaseModel):
+    updated_at:int
+    message: Optional[dict]
+    edited_message:Optional[dict]
+    channel_post:Optional[dict]
+    edited_channel_post: Optional[dict]
 
-# if __name__ == '__main__':
-#     main()
+    inline_query: Optional[dict]
+    chosen_inline_result: Optional[dict]
+    callback_query: Optional[dict]
+    shipping_query: Optional[dict]
+    pre_checkout_query: Optional[dict]
+    #poll request infos
+    poll: Optional[dict]
+    poll_answer: Optional[dict]
 
-from fastapi import FastAPI
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to my-horoscope bot!")
 
-app = FastAPI()
+def register_handlers(dispatcher):
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+@app.post("/webhook")
+def webhook(webhook_data:TelegramWebHook):
+    bot =Bot(token=TOKEN)
+    update = Update.de_json(webhook_data.__dict__,bot)
+    dispatcher = Dispatcher(bot,None,workers=4)
+    register_handlers(dispatcher=dispatcher)
+
+    dispatcher.process_update(update)
+    return {"message":"OK"}
 
 @app.get("/")
 def index():
     return {"message": "Hello World"}
+
+
+
+
+
